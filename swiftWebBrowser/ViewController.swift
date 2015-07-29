dfa970904f32f9ac04b2a4f9817b8b63b801e7de
daniel <danielnanuk@gmail.com>
2015-07-29 08:29:34 +0800

    WebView

--- swiftWebBrowser/ViewController.swift
+++ swiftWebBrowser/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIWebViewDelegate,UITextFieldDelegate {
 
+    @IBOutlet var webView : UIWebView?
+    @IBOutlet var textField : UITextField?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let myURL = NSURL(string:"http://daniel.eggpain.cn")
+        let request = NSURLRequest(URL: myURL!)
+        webView?.loadRequest(request)
+        
     }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 
+    @IBAction func textDone(){
+        let typedURL = textField?.text
+        
+        if((typedURL?.isEmpty) == nil){
+            return
+        }
+        
+        let myURL = NSURL(string: typedURL!)
+        let request = NSURLRequest(URL: myURL!)
+        webView?.loadRequest(request)
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
